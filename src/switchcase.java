4c34f2d2d45e2c55f67ec306229bd209b4d9480e
ovi2017 <oc210@email.vccs.edu>
2017-10-22 18:15:57 -0400

    Initial commit

---
+++ src/switchcase.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+//Ovidio Castillo
+//Purpose: To Display welcome messages for each choice of user
+//Date: 10/21/2017
+//License: GNU
+
+
+public class switchcase extends Application {
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Choose Account Type");
+        primaryStage.setWidth(300);
+
+        ObservableList<String> options = FXCollections.observableArrayList("Administrator", "Faculty", "Student", "Staff", "Guest");
+        ComboBox combobox = new ComboBox(options);
+        Label label = new Label("Greeting");
+        FlowPane pane = new FlowPane();
+        combobox.setOnAction(e->System.out.println("Welcome " + combobox.getValue()));
+        Scene scene = new Scene(combobox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }}
+
+
